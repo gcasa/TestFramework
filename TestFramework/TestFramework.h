1ac37be9c3a5456c4571ec16b9c8e4d0df6db81b
Gregory John Casamento <greg.casamento@gmail.com>
2022-08-28 07:51:55 -0400

    Update headers

--- TestFramework/TestFramework.h
+++ TestFramework/TestFramework.h
@@ -15,4 +15,4 @@ FOUNDATION_EXPORT const unsigned char TestFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <TestFramework/PublicHeader.h>
 
-
+#import <TestFramework/TestObject.h>
